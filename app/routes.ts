13a99399c789654e54242ab41f081ec79044bedb
huaguohao <810913476@qq.com>
2026-05-26 10:41:25 +0800

    set route

--- app/routes.ts
+++ app/routes.ts
@@ -2,10 +2,9 @@ import { type RouteConfig, index, route, prefix} from "@react-router/dev/routes"
 
 export default [
     index("page/home/home.tsx"),
-    route("crypto", "page/crypto/crypto.tsx"),
     ...prefix("crypto", [
         index("page/crypto/crypto.tsx"),
-        route(":id", "./page/crypto/exchange/exchange.tsx"),
+        route(":id", "page/crypto/exchange/exchange.tsx"),
     ]),
     route("polymarket", "page/polymarket/polymarket.tsx"),
     route("swap", "page/swap/swap.tsx"),
