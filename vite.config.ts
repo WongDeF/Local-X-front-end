d66b539f8e3ba8e2bf0e2724a190ac56ea84a58c
huaguohao <810913476@qq.com>
2026-06-30 11:59:38 +0800

    2

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,16 @@
 import { reactRouter } from "@react-router/dev/vite";
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
-
+import { resolve } from 'path'
 export default defineConfig({
   plugins: [tailwindcss(), reactRouter()],
   resolve: {
     tsconfigPaths: true,
+    alias: {
+      '~': resolve(__dirname, './app'),   // 关键修改
+      // 或者如果你的源码在 src 目录，就改成：
+      // '~': resolve(__dirname, './src'),
+    },
   },
   server: {
     host: '0.0.0.0',
