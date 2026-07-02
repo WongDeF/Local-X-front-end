037eab91f095ced73b70ac1d05b3d4b6318a23c4
huaguohao <810913476@qq.com>
2026-07-02 11:02:16 +0800

    修改vite

--- vite.config.ts
+++ vite.config.ts
@@ -3,14 +3,8 @@ import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
 import { resolve } from 'path';
 
-import { cloudflare } from "@cloudflare/vite-plugin";
-
 export default defineConfig({
-  plugins: [tailwindcss(), reactRouter(), cloudflare({
-    viteEnvironment: {
-      name: "ssr"
-    }
-  })],
+  plugins: [tailwindcss(), reactRouter()],
   resolve: {
     alias: {
       '~': resolve(__dirname, './app'),
